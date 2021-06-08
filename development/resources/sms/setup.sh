8ae1423aa54218b7c075ff96846d2695464a0cd7
Tanawit Kritwongwiman <tanawit139@gmail.com>
2021-06-08 18:06:03 +0700

    fix script path

--- development/resources/sms/setup.sh
+++ development/resources/sms/setup.sh
@@ -6,7 +6,7 @@ curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejs
 npm install -g yarn
 cp .env ./healthcare/backend/sms/.env
-cd healthcare/backend/sms
+cd healthcare/backend/sms-service
 yarn install
 yarn build
 yarn start:prod &
